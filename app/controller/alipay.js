f5da0008410b33f68c4c4b32e9840f64fb9dc64f
lucong <332553882@qq.com>
2019-12-03 16:30:12 +0800

    update

--- app/controller/alipay.js
+++ app/controller/alipay.js
@@ -1,4 +1,4 @@
-const Controller = require('../../lib/controller') 
+const Controller = require('../../lib/controller')
 const AlipaySdk = require('../../sdk/aplipaySdk')
 
 class AlipayController extends Controller {
@@ -9,17 +9,30 @@ class AlipayController extends Controller {
    * @param {*} ret 
    */
   async wapPay(args, ret) {
-    this.LOG.info(args.uuid, '/wapPay' , args)
+    this.LOG.info(args.uuid, '/wapPay', args)
     let outTradeNo = args.out_trade_no
     let totalAmount = parseFloat((args.amount / 100).toFixed(2))
     let body = args.body || ''
     let subject = args.subject
     let wapPayRet = await AlipaySdk.wapPay(outTradeNo, totalAmount, body, subject)
-    this.LOG.info(args.uuid, '/wapPay wapPayRet' , wapPayRet)
+    this.LOG.info(args.uuid, '/wapPay wapPayRet', wapPayRet)
     ret.data = {
       action: wapPayRet
     }
     return ret
+  }
+
+  async toAccountTransfer(args, ret) {
+    this.LOG.info(args.uuid, '/toAccountTransfer', args)
+    let outBizNo = args.out_biz_no
+    let payeeAccount = args.account
+    let amount = (args.amount / 100).toFixed(2)
+
+    let toAccountTransferRet = await AlipaySdk.toAccountTransfer(outBizNo, payeeAccount, amount)
+    this.LOG.info(args.uuid, '/toAccountTransfer toAccountTransferRet', toAccountTransferRet)
+
+    return toAccountTransferRet
+
   }
 }
 
