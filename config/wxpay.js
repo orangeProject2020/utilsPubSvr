67fc8fda1f525731c22e920eb4a53eb82669de03
lucong <332553882@qq.com>
2020-01-15 15:01:21 +0800

    update

--- config/wxpay.js
+++ config/wxpay.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   h5: {
-    app_id: 'wx81a4c1ea6e6076da',
+    app_id: 'wxe347dd259dc36edd',
     mch_id: '1573075611',
     key: 'dcc56b02438e78e756a2a07600a1af1f',
     notify_url: 'http://api.sunonenight.com/notify/wxpay'
