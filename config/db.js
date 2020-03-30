50aa0746dd6d77fde2542f8672028e2b91066daa
lucong <332553882@qq.com>
2020-03-30 16:14:42 +0800

    db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ module.exports = process.env.NODE_ENV === 'production' ? {
   port: 3306,
   dbname: 'db_base',
   username: 'root',
-  password: 'sbwd2020',
+  password: 'Sbwd_2020',
   maxLimit: 1000,
 } : {
   host: '59939c0a9a983.gz.cdb.myqcloud.com',
